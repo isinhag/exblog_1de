bf73798848cba585e09b8f03a4c54f4bb95f4117
isinhag <isabeladasilvagomes@gmail.com>
2023-10-19 09:14:12 -0300

    .

--- helpers/url.php
+++ helpers/url.php
@@ -3,6 +3,6 @@
 // Constrói a URL base do site combinando o protocolo, 
 // Nome do servido, diretório atual e barra final.
 
-$BASE_URL = "http://" .$_SERVER['SERVER_NAME'] .dirname($_SERVER["REQUEST_URI"].'?').'';
+$BASE_URL = "http://" .$_SERVER['SERVER_NAME'] .dirname($_SERVER["REQUEST_URI"].'?').'/';
 
 ?>
